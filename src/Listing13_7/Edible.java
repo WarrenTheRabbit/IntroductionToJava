37772632653fd5533cb1c8d5d26085938debc053
Warren <rabbitsinwarrens@gmail.com>
2021-04-19 10:45:49 +1000

    Understanding interfaces and abstract classes. Code used to answer Checkpoint 13.5.5

---
+++ src/Listing13_7/Edible.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listing13_7;
+
+/**
+ *
+ * @author Miranda and Warren
+ */
+public interface Edible {
+  /** Describe how to eat */
+  public abstract String howToEat();
+}
